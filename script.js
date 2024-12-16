2b25d5995d683a7ebf37ce5a28c048c62c84191c
JanicaJensen <122334098+JanicaJensen@users.noreply.github.com>
2024-12-15 17:36:18 -0700

    added a lot of javascript

--- script.js
+++ script.js
@@ -29,3 +29,36 @@ function displayDateTime() {
 displayDateTime();
 // updates every second
 setInterval(displayDateTime, 1000);
+
+// function to display current age
+function calculateAge() {
+  // takes the input from the birthday date box's value
+  const birthdayInput = document.getElementById("birthdayInput").value;
+  //if there is a birthday input, we need to...
+  if (birthdayInput) {
+    // create a constant called birthday using the date from the birthday input
+    const birthday = new Date(birthdayInput);
+    // set Now as a new date
+    const now = new Date();
+    // the getFullYear is built into Javascript, this is taking the year and subtracting from birthday years
+    let age = now.getFullYear() - birthday.getFullYear();
+    // this is taking the month and subtracting it from the current month
+    const monthDiff = now.getMonth() - birthday.getMonth();
+
+    // checking if their birthday has happened this year yet!
+    if (
+      // if monthDiff is negative, it means the current month is before birthday month
+      monthDiff < 0 ||
+      (monthDiff === 0 && now.getDate() < birthday.getDate())
+    ) {
+      age--;
+    }
+    document.getElementById("age").innerText = `They are ${age} years old`;
+  } else {
+    document.getElementById("age").innerText = "Please enter a DOB";
+  }
+}
+
+document
+  .getElementById("birthdayInput")
+  .addEventListener("input", calculateAge);
